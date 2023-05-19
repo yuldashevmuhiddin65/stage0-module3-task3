40329d86c736e24ead0c9e4a25e6be3f72169ede
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-19 16:15:37 +0500

    module 3 task 3

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -3,5 +3,14 @@ package lang.print.gaps.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int a = 1;
+        int b = 10;
+        int c = 100;
+        a = 15;
+        b = 6;
+        c = 4;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
